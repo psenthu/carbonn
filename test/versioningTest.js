f9c25699799f2d3b6b8fd7de930cc5829a6af33a
Senthuraan Ponnampalam <psenthu@gmail.com>
2013-03-09 15:29:31 +0530

    test file included with example test

---
+++ test/versioningTest.js
@@ -0,0 +1,9 @@
+var should    = require('should');
+
+
+describe('test versioning symatics',function(done){
+	it('should return true',function(done){
+		true.should.be.equal(true);
+		done();
+	});
+});
